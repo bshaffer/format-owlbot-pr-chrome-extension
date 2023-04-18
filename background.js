51a6fcb42a9524911e570bbf144d4c2ebf283144
Brent Shaffer <betterbrent@google.com>
2023-04-18 12:20:16 -0700

    make extension available outside google-cloud-php

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-const github = 'https://github.com/googleapis/google-cloud-php/pull/';
+const prregex = /https:\/\/github.com\/googleapis\/.*\/pull\//;
 chrome.action.onClicked.addListener(() => {
     chrome.tabs.query({active: true, currentWindow: true}).then(([tab]) => {
-        if (tab.url.startsWith(github)) {
+        if (prregex.test(tab.url)) {
             chrome.scripting.executeScript({
                 target: {tabId: tab.id},
                 files: ['formatpr.js'],
